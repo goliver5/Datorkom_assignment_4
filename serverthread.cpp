ddb93f71130b98c3e0cef7eaa3f0f4e45ba530ad
Oliver <oliver_msn94@hotmail.com>
2022-10-19 16:24:39 +0200

    started with looking through a char[] for a char

--- serverthread.cpp
+++ serverthread.cpp
@@ -3,10 +3,46 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <cstring>
 
 /* You will to add includes here */
 
+//returns the amount of chars in the char*[]
+int checkForChar(char p[], const char searchingFor = '/')
+{
+  int size = strlen(p);
+  int counter = 0;
+  for (int i = 0; i < size; i++)
+  {
+      if(p[i] == searchingFor)
+      {
+        counter++;
+      }
+  }
+  return counter;
+}
 
+int checkForCharPointerAddition( char looking[], const char searchingFor = '/')
+{
+  int counter = 0;
+  char* p =nullptr;
+  printf("searchingFor: %c\n",searchingFor);
+  p = (char*)malloc(strlen(looking) * sizeof(char));
+  memcpy(p,looking,strlen(looking));
+  char* startPointer = p;
+
+  for(int i = 0; i< strlen(looking); i++)
+  {
+    if(*p == searchingFor)
+    {
+      counter++;
+    }
+    p++;
+  }
+  printf("free ");
+  free(startPointer);
+  return counter;
+}
 
 using namespace std;
 
@@ -15,8 +51,12 @@ int main(int argc, char *argv[]){
   /* Do more magic */
 
 
-  
-  printf("done.\n");
+  printf("strlen of argv[1]: %d\n",strlen(argv[1]));
+
+
+  int nrOfSlashes = checkForChar(argv[1], '/');
+  //int nrOfSlashes = checkForCharPointerAddition(argv[1]);
+  printf("nrofSlashes: %d.\n", nrOfSlashes);
   return(0);
 
 
